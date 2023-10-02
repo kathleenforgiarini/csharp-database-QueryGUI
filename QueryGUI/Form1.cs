0b8bcbcd4735197c8469627bfc5c533a0f0fe5fc
kathleenforgiarini <kath.for199@gmail.com>
2023-10-02 16:27:15 -0400

    Add project files.

---
+++ QueryGUI/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace QueryGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnectionStringBuilder cs = new SqlConnectionStringBuilder();
+            cs.DataSource = "(local)";
+            cs.InitialCatalog = "EMP";
+            cs.UserID = "sa";
+            cs.Password = "sysadm";
+
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM COMPANY", cs.ConnectionString);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "COMPANY");
+
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            dataGridView1.DataSource = ds.Tables["COMPANY"];
+        }
+    }
+}
